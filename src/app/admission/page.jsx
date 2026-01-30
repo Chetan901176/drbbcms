1744e93ad705aae99bd65a698e5e62de60ee2c09
FaizShaikh27 <faiz008361@gmail.com>
2026-01-30 15:23:17 +0530

    changed whatsapp to alternate

--- src/app/admission/page.jsx
+++ src/app/admission/page.jsx
@@ -317,7 +317,7 @@ export default function AdmissionPage() {
                         className="block text-gray-700 mb-2 font-medium text-sm sm:text-base"
                         htmlFor="whatsappPhone"
                       >
-                        WhatsApp No. *
+                        Alternate Phone Number *
                       </label>
                       <input
                         type="tel"
@@ -328,7 +328,7 @@ export default function AdmissionPage() {
                         pattern="[0-9]{10}"
                         required
                         className="w-full px-3 sm:px-4 py-2 sm:py-3 text-sm sm:text-base border border-gray-300 rounded-lg focus:ring-2 focus:ring-green-600 focus:border-transparent outline-none transition-all"
-                        placeholder="10-digit WhatsApp number"
+                        placeholder="10-digit alternate number"
                       />
                     </div>
 
