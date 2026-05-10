809581e2242194887ed048a8b68118f1402d9213
FaizShaikh27 <faiz008361@gmail.com>
2026-05-10 11:43:30 +0530

    comment out carosel

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -19,7 +19,7 @@ const page = () => {
     <HeroSection/>
     <PromoVideo/>
     <AboutSection/>
-    <Carousel/>
+    {/* <Carousel/> */}
     <CampusSection/>
     <DirectorsMessageSection/>
     <TestimonialsSlider/>
