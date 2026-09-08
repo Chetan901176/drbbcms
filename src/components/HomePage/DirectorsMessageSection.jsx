ff274e80dd5776e7435d2e272c09795a9fa7ce74
Chetan901176 <makemefly777@outlook.com>
2026-09-08 17:02:43 +0530

    Update director's photo source in DirectorsMessageSection

--- src/components/HomePage/DirectorsMessageSection.jsx
+++ src/components/HomePage/DirectorsMessageSection.jsx
@@ -15,7 +15,7 @@ const DirectorsMessageSection = () => {
           <div className="w-full lg:w-1/3 flex-shrink-0">
             <Image
               className="w-full h-auto rounded-xl shadow-md"
-              src="/Wakle_Sir_Picture_On_School_Web.jpeg"
+              src="/Wakle_Sir_Photo.png"
               alt="Dr. Adinath Wakle, Director"
               width={400}
               height={500}
