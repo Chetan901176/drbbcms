fb6e3d160e6c08abe001c7902b8825dcc25d9855
Chetan901176 <makemefly777@outlook.com>
2026-09-13 15:21:23 +0530

    Add 'use client' directive to video page

--- src/app/gallery/video/page.jsx
+++ src/app/gallery/video/page.jsx
@@ -97,6 +97,7 @@
 
 // export default VideoGallery;
 
+"use client";
 import React, { useState, useRef, useEffect } from "react";
 import { Home } from "lucide-react";
 import Link from "next/link";
